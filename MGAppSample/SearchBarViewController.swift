a417e6939ca9d222a24bfab08362d1e8d50dbb49
jasonSOUI <jason@soui.com.tw>
2018-07-25 17:28:54 +0800

    commit

--- MGAppSample/SearchBarViewController.swift
+++ MGAppSample/SearchBarViewController.swift
@@ -8,17 +8,84 @@
 
 import UIKit
 
-class SearchBarViewController: UIViewController {
-
+class SearchBarViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
+    
+    @IBOutlet weak var resultTableView: UITableView!
+    
+    var animalList = [Animal]()
+    
+    var currentAnimalList = [Animal]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpAnimalData()
+        createSearchBar()
+        
+        currentAnimalList = animalList
     }
 
+    func setUpAnimalData() {
+        animalList.append(Animal(name: "Aenescumb", image: "Aenescumb"))
+        animalList.append(Animal(name: "Akker", image: "Akker"))
+        animalList.append(Animal(name: "Akker", image: "Akker"))
+        animalList.append(Animal(name: "Baram", image: "Baram"))
+        animalList.append(Animal(name: "Dinsmore", image: "Dinsmore"))
+        animalList.append(Animal(name: "SBtinka", image: "SBtinka"))
+        animalList.append(Animal(name: "Tylere", image: "Tylere"))
+        animalList.append(Animal(name: "Ollaneg", image: "Ollaneg"))
+        animalList.append(Animal(name: "Mirabelle", image: "Mirabelle"))
+        animalList.append(Animal(name: "Jabbar", image: "Jabbar"))
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        if(searchText == "") {
+            currentAnimalList = animalList
+        } else {
+            currentAnimalList = animalList.filter({ (animal) -> Bool in
+                animal.name.lowercased().contains(searchText.lowercased())
+            })
+        }
+        resultTableView.reloadData()
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, selectedScopeButtonIndexDidChange selectedScope: Int) {
+        
+    }
+    
+    func createSearchBar() {
+        
+        let searchBar = UISearchBar()
+        searchBar.showsCancelButton = false
+        searchBar.placeholder = "Enter Name.."
+        searchBar.delegate = self
+        
+        self.navigationItem.titleView = searchBar
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
-
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return currentAnimalList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "searchBarTableCell") {
+            cell.textLabel?.text = currentAnimalList[indexPath.row].name
+            return cell
+        } else {
+            return UITableViewCell()
+        }
+    }
+    
+    
+    
     /*
     // MARK: - Navigation
 
@@ -29,4 +96,9 @@ class SearchBarViewController: UIViewController {
     }
     */
 
+}
+
+struct Animal {
+    var name: String
+    var image: String
 }
